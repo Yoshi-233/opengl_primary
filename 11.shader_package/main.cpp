cdfe6e10b51104a29695a04d66084175ff7fd489
yoshi.shao <yoshi.shao@nio.com>
2024-08-21 16:11:47 +0800

    add shader package

---
+++ 11.shader_package/main.cpp
@@ -0,0 +1,126 @@
+#include "glframework/include/core.h"
+#include "glframework/include/shader.h"
+#include "application/Application.h"
+#include "common/include/check_err.h"
+#include "common/include/common.h"
+
+GLuint vao = 0;
+
+// 窗口大小改变回调函数
+void OnResize(GLFWwindow *window, int width, int height)
+{
+        GL_CHECK_ERR(glViewport(0, 0, width, height));
+}
+
+/*
+ * key_callback() 函数用于处理键盘事件
+ * 参数：
+ * GLFWwindow *window：当前窗口的句柄
+ * int key：按下的键的键码
+ * int scancode：按下的键的扫描码, 物理按键码, 有些键盘排布不一致，根据位置推算按键，一般不用
+ * int action：键的动作，如按下或释放
+ * int mode：当前窗口的输入模式， 如shift、ctrl等
+ * */
+void OnKeyBoard(GLFWwindow *window, int key, int scancode, int action, int mode)
+{
+        if ((key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) ||
+            (key == GLFW_KEY_C && action == GLFW_RELEASE && mode == GLFW_MOD_CONTROL)) {
+                glfwSetWindowShouldClose(window, GLFW_TRUE);
+        }
+}
+
+void prepareVao()
+{
+        float positions[] = {
+                -0.5f, -0.5f, 0.0f,
+                0.5f, -0.5f, 0.0f,
+                0.0f, 0.5f, 0.0f,
+                0.5f, 0.5f, 0.0f
+        };
+
+        float colors[] = {
+                1.0f, 0.0f, 0.0f,
+                0.0f, 1.0f, 0.0f,
+                0.0f, 0.0f, 1.0f
+        };
+
+        u32 indices[] = {
+                0, 1, 2,
+                2, 1, 3
+        };
+
+        /* create vbo */
+        GLuint posVbo, colorVbo;
+        glGenBuffers(1, &posVbo);
+        glBindBuffer(GL_ARRAY_BUFFER, posVbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+        glGenBuffers(1, &colorVbo);
+        glBindBuffer(GL_ARRAY_BUFFER, colorVbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+        /* create ebo */
+        GLuint ebo;
+        glGenBuffers(1, &ebo);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+        /* create vao */
+        glGenVertexArrays(1, &vao);
+        glBindVertexArray(vao);
+
+        /* bind vbo/ebo */
+        glBindBuffer(GL_ARRAY_BUFFER, posVbo);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void *)0);
+        glBindBuffer(GL_ARRAY_BUFFER, colorVbo);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void *)0);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+
+        glBindVertexArray(0);
+}
+
+void prepareShader()
+{
+        shader = new Shader("assets/shaders/vertex.glsl", "assets/shaders/fragment.glsl");
+}
+
+void render()
+{
+        // 清屏
+        GL_CHECK_ERR(glClear(GL_COLOR_BUFFER_BIT));
+        shader->begin();
+        glBindVertexArray(vao);
+
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
+        // 这里最好解绑，这样误操作就不会影响当前vao
+        Shader::end();
+}
+
+int main()
+{
+        cpp_init();
+        if (!APP.init(800, 600, "LearnOpenGL")) {
+                ERROR( "Failed to initialize Application");
+                return -1;
+        }
+
+        APP.setResizeCallback(OnResize);
+        APP.setKeyCallback(OnKeyBoard);
+
+        // opengl渲染的区域
+        GL_CHECK_ERR(glViewport(0, 0, 800, 600));
+        // 设置清屏颜色
+        GL_CHECK_ERR(glClearColor(0.2f, 0.3f, 0.3f, 1.0f));
+
+        prepareVao();
+        prepareShader();
+
+        while (APP.update()) {
+                render();
+                // 绘制代码
+        }
+        APP.close();
+        return 0;
+}
