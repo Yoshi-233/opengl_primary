fd3f85208102f3872e1bffe666bc0a46bf25b492
yoshi.shao <yoshi.shao@nio.com>
2024-09-04 11:40:13 +0800

    add texture blend

---
+++ 17.texture_blend/assets/shaders/vertex.glsl
@@ -0,0 +1,16 @@
+#version 410 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aUV;
+
+out vec3 color;
+out vec2 uv;
+
+uniform float time;
+
+void main()
+{
+        gl_Position = vec4(aPos, 1.0);
+        color = aColor;
+        uv = aUV;
+}
